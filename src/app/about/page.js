1dd9fc1c7344afe8c2795a14af0913728b3c8e8e
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-07-14 19:31:18 +0530

    Made HomePage and AboutPage

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import Herosection from "../components/Herosection";
+
+const About = () => {
+  return (
+    <>
+      <Herosection title="Our Story" imageUrl="/about1.svg" />
+    </>
+  );
+};
+
+export default About;
