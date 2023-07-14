4e4f825852db4f96842fb77a9323075cd8115694
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-07-14 21:44:45 +0530

    Handled Loading

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -4,6 +4,7 @@ import MovieCard from "../components/MovieCard";
 import styles from "@/app/styles/common.module.css";
 
 const Movie = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   const url = process.env.RAPID_URL;
   const options = {
     method: "GET",
