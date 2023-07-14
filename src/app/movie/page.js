f024c4f033005fdaf7df4e4fcc51b16d02997b50
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-07-14 21:32:37 +0530

    Made MoviePage

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -1,11 +1,35 @@
+import Link from "next/link";
 import React from "react";
+import MovieCard from "../components/MovieCard";
+import styles from "@/app/styles/common.module.css";
 
-const Page = () => {
+const Movie = async () => {
+  const url = process.env.RAPID_URL;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "68b8659992msh436fae9e6e784d1p125ff2jsn0e47a2b7d8b5",
+      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+    },
+  };
+  const res = await fetch(url, options);
+  const data = await res.json();
+  const main_data = data.titles;
+  console.log(data);
   return (
     <>
-      <h1>Movie Page</h1>
+      <section className={styles.movieSection}>
+        <div className={styles.container}>
+          <h1>Series and Movies</h1>
+          <div className={styles.card_section}>
+            {main_data.map((curElem) => {
+              return <MovieCard key={curElem.id} {...curElem} />;
+            })}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
 
-export default Page;
+export default Movie;
